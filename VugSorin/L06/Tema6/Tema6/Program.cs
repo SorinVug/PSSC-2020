79075a883e55c8fe8849d189717e08e970e7df61
SorinVug <53868105+SorinVug@users.noreply.github.com>
2020-11-05 18:27:00 +0200

    RezolvareTema6

---
+++ VugSorin/L06/Tema6/Tema6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LanguageExt;
+using Tema6.Outputs;
+
+namespace Tema6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wf = from createReplyResult in Domain.ValidateReply(1, 1, "test")
+                     let validReply = (CreateReplyResult.ReplyValid)createReplyResult
+                     from checkLanguageResult in Domain.CheckLanguage(validReply.Reply.Answer)
+                     from ownerAck in Domain.SendAckToOwner(checkLanguageResult)
+                     from authorAck in Domain.SendAckToAuthor(checkLanguageResult)
+                     select (validReply, checkLanguageResult, ownerAck, authorAck);
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+    internal interface IReplyPosted
+    {
+    }
+}
